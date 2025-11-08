b0ff1c9b373bf6a15a2c832a89043ab40af7f2bd
Makiya Crochiere <crochieremakiya@gmail.com>
2025-11-08 13:47:56 -0600

    updated puzzle3

--- src/pages/Puzzle3.js
+++ src/pages/Puzzle3.js
@@ -60,11 +60,7 @@ function Puzzle3({ updateToken }) {
         <div className="cipher-section">
           <div className="cipher-label">Encoded Message:</div>
           <div className="puzzle-text" style={{ fontFamily: 'monospace', fontSize: '0.9rem' }}>
-            G U I L I V Y Y F V W V L I K F Z J G Z L A F W V F G Y Z J I V F V Y D M V I V Z F R U W G I{'\n'}
-            N V J J Z L Y V G W F G F I N L E G Y Z J X V G W V I V R G F Y J G J W V Y T M P R G Y F{'\n'}
-            Z E J F L L Z L P Z Z L D V I M F V M G Z L Y V M P Y T I V T Y V V G Z F N V Z D V F N V F{'\n'}
-            V Z E L P I V G I V U F E U F Y T Z M V I L L U D M V I V Z M I V V W G A V J I V J Z D M G Z{'\n'}
-            W L F L I G Y T J M G X V J M G I V J Z M V F I Y V J Z
+            G U I L I V Y Y F V W V L I K F Z J G Z L A F W V F G Y Z J I V F V Y D M V I V Z F R U W G I N V J J Z L Y V G W F G F I N L E G Y Z J X V G W V I V R G F Y J G J W V Y T M P R G Y F Z E J F L L Z L P Z Z L D V I M F V M G Z L Y V M P Y T I V T Y V V G Z F N V Z D V F N V F V Z E L P I V G I V U F E U F Y T Z M V I L L U D M V I V Z M I V V W G A V J I V J Z D M G Z W L F L I G Y T J M G X V J M G I V J Z M V F I Y V J Z
           </div>
         </div>
 
